276234d823e5f4c5b0b634700aee2905a1599437
Mr sumanth <sumanthmr423@gmail.com>
2026-06-27 14:48:39 +0530

    feat: bypass 3D initialization failure to guarantee HTML UI rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useState, useEffect } from "react";
 import "./App.css";
 
 const CharacterModel = lazy(() => import("./components/Character"));
 const MainContainer = lazy(() => import("./components/MainContainer"));
 import { LoadingProvider } from "./context/LoadingProvider";
+
+const ClientCharacterModel = () => {
+  const [isBrowser, setIsBrowser] = useState(false);
+  useEffect(() => setIsBrowser(true), []);
+  if (!isBrowser) return null;
+  return <CharacterModel />;
+};
 
 const App = () => {
   return (
@@ -12,7 +19,7 @@ const App = () => {
         <Suspense>
           <MainContainer>
             <Suspense>
-              <CharacterModel />
+              <ClientCharacterModel />
             </Suspense>
           </MainContainer>
         </Suspense>
